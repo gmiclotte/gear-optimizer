69cfe54c5bfa1633cd6cb24e084f935636e3cbab
Giles Miclotte <giles.miclotte@ugent.be>
2019-06-18 08:50:23 +0200

    Updates

---
+++ precache-manifest.195d800a7f2cf88a0268e18e6893a1b4.js
@@ -0,0 +1,386 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f7a10b64f81c2291249b1f6a266e0a",
+    "url": "/gear-optimizer/38018717cf12f5bbfc2b.worker.js"
+  },
+  {
+    "revision": "b4d22088eac011a6e1bece74a7a0d4ca",
+    "url": "/gear-optimizer/index.html"
+  },
+  {
+    "revision": "7b61c860b3a9268f9643",
+    "url": "/gear-optimizer/static/css/main.d1e040c7.chunk.css"
+  },
+  {
+    "revision": "f3f92018f2965e78b72c",
+    "url": "/gear-optimizer/static/js/2.397b66df.chunk.js"
+  },
+  {
+    "revision": "7b61c860b3a9268f9643",
+    "url": "/gear-optimizer/static/js/main.4dabc0e0.chunk.js"
+  },
+  {
+    "revision": "e70c69565185911edd94",
+    "url": "/gear-optimizer/static/js/runtime~main.5e32c41c.js"
+  },
+  {
+    "revision": "4bd16a154af625f304df477ca4361c80",
+    "url": "/gear-optimizer/static/media/A Bald Egg.4bd16a15.png"
+  },
+  {
+    "revision": "bb8ccd1294cb6dd7f05baa9a6147a991",
+    "url": "/gear-optimizer/static/media/A Beanie.bb8ccd12.png"
+  },
+  {
+    "revision": "85b09c2d25a127cf2adbffc036001b35",
+    "url": "/gear-optimizer/static/media/A Giant Apple.85b09c2d.png"
+  },
+  {
+    "revision": "fd90b7834fd4a8efca4a4f4df31409b3",
+    "url": "/gear-optimizer/static/media/A Giant Bazooka.fd90b783.png"
+  },
+  {
+    "revision": "17e99dbca3e4dc0eaa5aed14622c94c6",
+    "url": "/gear-optimizer/static/media/A Power Pill.17e99dbc.png"
+  },
+  {
+    "revision": "f8f00704135b5d05ea13ddde572bf8b9",
+    "url": "/gear-optimizer/static/media/A Regular Tie.f8f00704.png"
+  },
+  {
+    "revision": "cd1894f72fa900adf8ff97383e55c086",
+    "url": "/gear-optimizer/static/media/A Small Gerbil.cd1894f7.png"
+  },
+  {
+    "revision": "99aaabd964c0ade630cf6d270995561c",
+    "url": "/gear-optimizer/static/media/Amulet of Sunshin.99aaabd9.png"
+  },
+  {
+    "revision": "f4cfaf5974d5056be814c7e5e60bc84b",
+    "url": "/gear-optimizer/static/media/An Infinitely Long Strand of Beard Hair.f4cfaf59.png"
+  },
+  {
+    "revision": "a5ecc7d210b9f479e0bc0d7c117d6b1b",
+    "url": "/gear-optimizer/static/media/Ascended Ascended Ascended Pendant.a5ecc7d2.png"
+  },
+  {
+    "revision": "ffdc5edc1c4dff88436c8ebe11d59577",
+    "url": "/gear-optimizer/static/media/Ascended Ascended Forest Pendant.ffdc5edc.png"
+  },
+  {
+    "revision": "c88e565e2373c7e2dc52b27b4990a614",
+    "url": "/gear-optimizer/static/media/Ascended x4 Pendant.c88e565e.png"
+  },
+  {
+    "revision": "51846018566ccb10c9a242eb3f9fc241",
+    "url": "/gear-optimizer/static/media/Badly Drawn Chest.51846018.png"
+  },
+  {
+    "revision": "711abcdbeefc01b51acd53672a98529a",
+    "url": "/gear-optimizer/static/media/Badly Drawn Foot.711abcdb.png"
+  },
+  {
+    "revision": "e2c33a748f2dbb77c4614fc9cb3d06f6",
+    "url": "/gear-optimizer/static/media/Badly Drawn Gun.e2c33a74.png"
+  },
+  {
+    "revision": "acc4ea7683712531acff4b862a64da12",
+    "url": "/gear-optimizer/static/media/Badly Drawn Pants.acc4ea76.png"
+  },
+  {
+    "revision": "281dced5699eba249cc864573c4cbb40",
+    "url": "/gear-optimizer/static/media/Badly Drawn Smiley Face.281dced5.png"
+  },
+  {
+    "revision": "0bb9ece4a652c7e1ce6e8e3bfe088b5f",
+    "url": "/gear-optimizer/static/media/Beam Laser Sword.0bb9ece4.png"
+  },
+  {
+    "revision": "3e849dd6f410d553c59cc9aa15428461",
+    "url": "/gear-optimizer/static/media/Bearded Axe.3e849dd6.png"
+  },
+  {
+    "revision": "f89577abf39d4c2c2afb6557492a792b",
+    "url": "/gear-optimizer/static/media/Blue Heart.f89577ab.png"
+  },
+  {
+    "revision": "63898ed15ca0098aa8c2b78a6ba1b6fa",
+    "url": "/gear-optimizer/static/media/Braided Beard Legs.63898ed1.png"
+  },
+  {
+    "revision": "24dc282611af9f3959b9789a8489574d",
+    "url": "/gear-optimizer/static/media/Brown Heart.24dc2826.png"
+  },
+  {
+    "revision": "46868441af1b830082de54271c6823de",
+    "url": "/gear-optimizer/static/media/Chocolate Crowbar.46868441.png"
+  },
+  {
+    "revision": "4b671158bdc846acd264fe628ceededb",
+    "url": "/gear-optimizer/static/media/Circle Helmet.4b671158.png"
+  },
+  {
+    "revision": "3c86ff4c07d63c645aaad16f3f5ffab3",
+    "url": "/gear-optimizer/static/media/Cursed Ring.3c86ff4c.png"
+  },
+  {
+    "revision": "473e30e31520ea6a00d922bdddc08303",
+    "url": "/gear-optimizer/static/media/Dorky Glasses.473e30e3.png"
+  },
+  {
+    "revision": "df23537af68c41e9ae9bc0e4b9ae7c36",
+    "url": "/gear-optimizer/static/media/Dragon Wings.df23537a.png"
+  },
+  {
+    "revision": "b7f48c4cd5a5bfd5cd636153cd32396e",
+    "url": "/gear-optimizer/static/media/Fanny Pack.b7f48c4c.png"
+  },
+  {
+    "revision": "a845b41dabe48b06e342f6a35a6b56ef",
+    "url": "/gear-optimizer/static/media/Fuzzy Orange Cheeto Slippers.a845b41d.png"
+  },
+  {
+    "revision": "e2ed26027f5785cd99ee06d9a2ec50eb",
+    "url": "/gear-optimizer/static/media/Gaudy Boots.e2ed2602.png"
+  },
+  {
+    "revision": "6411c610e89b940279ed7be5ac935178",
+    "url": "/gear-optimizer/static/media/Gaudy Hat.6411c610.png"
+  },
+  {
+    "revision": "df87735506363be6b8218db141c20701",
+    "url": "/gear-optimizer/static/media/Gaudy Pants.df877355.png"
+  },
+  {
+    "revision": "e4b3627cc51de03fe65aeb3f219f3400",
+    "url": "/gear-optimizer/static/media/Gaudy Shirt.e4b3627c.png"
+  },
+  {
+    "revision": "a80c4382bd2925775645b48954017ae7",
+    "url": "/gear-optimizer/static/media/Generic Paperweight.a80c4382.png"
+  },
+  {
+    "revision": "0eaef6014b400109b94abba069c98557",
+    "url": "/gear-optimizer/static/media/Ghostly Chest.0eaef601.png"
+  },
+  {
+    "revision": "8ad8583f79382b2fa6cc3e07ba11019d",
+    "url": "/gear-optimizer/static/media/Gossamer Chest.8ad8583f.png"
+  },
+  {
+    "revision": "c89e7ceee7d1bff091c1234e8290bef0",
+    "url": "/gear-optimizer/static/media/Green Heart.c89e7cee.png"
+  },
+  {
+    "revision": "01d3dec9c11404698e21b47aea73110a",
+    "url": "/gear-optimizer/static/media/Groucho Marx Disguise.01d3dec9.png"
+  },
+  {
+    "revision": "ee59a363961dd3e85d265c57a5c3f82c",
+    "url": "/gear-optimizer/static/media/High Heeled Boots.ee59a363.png"
+  },
+  {
+    "revision": "54c7792bf8b657723e723a9f6f2aba89",
+    "url": "/gear-optimizer/static/media/King Circle's Amulet of Helping Random Stuff.54c7792b.png"
+  },
+  {
+    "revision": "0b8e27fbcf6a29d85d9c3ee14877b651",
+    "url": "/gear-optimizer/static/media/King Looty.0b8e27fb.png"
+  },
+  {
+    "revision": "cebe72bac4d5fa4cf25e3130cfc836f1",
+    "url": "/gear-optimizer/static/media/Mega Blue Jeans.cebe72ba.png"
+  },
+  {
+    "revision": "8f935dde206200c2a47b049edd2d51d7",
+    "url": "/gear-optimizer/static/media/Mega Boots.8f935dde.png"
+  },
+  {
+    "revision": "4c18c7e30ff0188ea28fa94cc28cb56b",
+    "url": "/gear-optimizer/static/media/Mega Chest.4c18c7e3.png"
+  },
+  {
+    "revision": "9d0d8f79ac2cd52d9f1d47d1cfaa1f1f",
+    "url": "/gear-optimizer/static/media/Mega Helmet.9d0d8f79.png"
+  },
+  {
+    "revision": "835f0d28132681222566df0faa7320f9",
+    "url": "/gear-optimizer/static/media/No Pants.835f0d28.png"
+  },
+  {
+    "revision": "5c049162ceebe71dfe5eecb79221b5f4",
+    "url": "/gear-optimizer/static/media/Office Hat.5c049162.png"
+  },
+  {
+    "revision": "6bc1d90bd0383872a569b67cf348fe7e",
+    "url": "/gear-optimizer/static/media/Office Pants.6bc1d90b.png"
+  },
+  {
+    "revision": "a157d3b84404f31c9e47593457620b21",
+    "url": "/gear-optimizer/static/media/Office Shirt.a157d3b8.png"
+  },
+  {
+    "revision": "0a8dc3988bf7759b774fc0270445e4f9",
+    "url": "/gear-optimizer/static/media/Office Shoes.0a8dc398.png"
+  },
+  {
+    "revision": "74c57bd8e6f7a62595e82de1973dbb85",
+    "url": "/gear-optimizer/static/media/Pants of Horror.74c57bd8.png"
+  },
+  {
+    "revision": "4927e5662b0735154e9c14bcb34a443a",
+    "url": "/gear-optimizer/static/media/Paper Fan.4927e566.png"
+  },
+  {
+    "revision": "d7c832d3eb673e18dfa1eef537eefeba",
+    "url": "/gear-optimizer/static/media/Polygon Boots.d7c832d3.png"
+  },
+  {
+    "revision": "84ee86890d264145618a9cfc2a04a70f",
+    "url": "/gear-optimizer/static/media/Rectangle Pants.84ee8689.png"
+  },
+  {
+    "revision": "4f4e024db1a5480816f4c6db84017b38",
+    "url": "/gear-optimizer/static/media/Red Heart.4f4e024d.png"
+  },
+  {
+    "revision": "fc6825fc208508d5f79a495aa15e4beb",
+    "url": "/gear-optimizer/static/media/Ring of Greed.fc6825fc.png"
+  },
+  {
+    "revision": "6b5aa943e3823af1c7be39bd8acc8194",
+    "url": "/gear-optimizer/static/media/Ring of Might.6b5aa943.png"
+  },
+  {
+    "revision": "a836116872a9f3881650ea34eb39fb94",
+    "url": "/gear-optimizer/static/media/Ring of Utility.a8361168.png"
+  },
+  {
+    "revision": "53aed2f1215c9c7b328583540112ee78",
+    "url": "/gear-optimizer/static/media/Ring of Way Too Much Energy.53aed2f1.png"
+  },
+  {
+    "revision": "5c2f65f62d6c49c297c11ecfd8537318",
+    "url": "/gear-optimizer/static/media/Ring of Way Too Much Magic.5c2f65f6.png"
+  },
+  {
+    "revision": "d2e073ee611e20ce54a17b814fe1b4e6",
+    "url": "/gear-optimizer/static/media/Sir Looty McLootington II.d2e073ee.png"
+  },
+  {
+    "revision": "70fe5e5c290cf3d77eafafef25632a48",
+    "url": "/gear-optimizer/static/media/Slimy Boots.70fe5e5c.png"
+  },
+  {
+    "revision": "69781dac883556d8a2a51d9403037d1e",
+    "url": "/gear-optimizer/static/media/Slimy Chest.69781dac.png"
+  },
+  {
+    "revision": "2cf9c1a3f2531cd799adda6c35bdf846",
+    "url": "/gear-optimizer/static/media/Slimy Helmet.2cf9c1a3.png"
+  },
+  {
+    "revision": "caa58d078266656cae96efcdc25ffbe7",
+    "url": "/gear-optimizer/static/media/Slimy Pants.caa58d07.png"
+  },
+  {
+    "revision": "be27e6da20f4c9755fa9aee437691993",
+    "url": "/gear-optimizer/static/media/Spectral Boots.be27e6da.png"
+  },
+  {
+    "revision": "20666838acc5763cad3c1be76f2c7841",
+    "url": "/gear-optimizer/static/media/Spooky Sword.20666838.png"
+  },
+  {
+    "revision": "faf9057a26c8e1166a3aa865db79abf6",
+    "url": "/gear-optimizer/static/media/Spoopy Helmet.faf9057a.png"
+  },
+  {
+    "revision": "cb2dd720958d15ef3c4c8083a5fd578f",
+    "url": "/gear-optimizer/static/media/Square Chestpiece.cb2dd720.png"
+  },
+  {
+    "revision": "2b319e7fdd4064c4c5e2ce1864f026ea",
+    "url": "/gear-optimizer/static/media/Stapler.2b319e7f.png"
+  },
+  {
+    "revision": "052d04b15f8801ff61b6ae617c5801f5",
+    "url": "/gear-optimizer/static/media/Stealthy Chest.052d04b1.png"
+  },
+  {
+    "revision": "8b848a7edd9e049bf67de06fc79a5337",
+    "url": "/gear-optimizer/static/media/Stealthy Hat.8b848a7e.png"
+  },
+  {
+    "revision": "49865d9c41b90b4144816bb0e575b403",
+    "url": "/gear-optimizer/static/media/THE CUBE.49865d9c.png"
+  },
+  {
+    "revision": "b9df05680fe2c377e3b931533d32d596",
+    "url": "/gear-optimizer/static/media/The Candy Cane of Destiny.b9df0568.png"
+  },
+  {
+    "revision": "5c37aba545db3173a4282e34b03294c3",
+    "url": "/gear-optimizer/static/media/The Fists of Flubber.5c37aba5.png"
+  },
+  {
+    "revision": "d2ab5868d324599406769649db165dcc",
+    "url": "/gear-optimizer/static/media/The Pen-Is.d2ab5868.png"
+  },
+  {
+    "revision": "5e2f284d4e15b7349a583e00f5194d89",
+    "url": "/gear-optimizer/static/media/The Stealthiest Armour.5e2f284d.png"
+  },
+  {
+    "revision": "9f18c56bfab4efbf4f492fee0a05f291",
+    "url": "/gear-optimizer/static/media/The Triple Flubber.9f18c56b.png"
+  },
+  {
+    "revision": "a53e411156abda29578e9dbb82653684",
+    "url": "/gear-optimizer/static/media/UUG's 'Special' Ring.a53e4111.png"
+  },
+  {
+    "revision": "e60b30d2dd326cc426303993124c018f",
+    "url": "/gear-optimizer/static/media/Wanderer's Boots.e60b30d2.png"
+  },
+  {
+    "revision": "54802a6201eca655da196b393b888636",
+    "url": "/gear-optimizer/static/media/Wanderer's Cane.54802a62.png"
+  },
+  {
+    "revision": "eb118d3a8819c62d3098fe85de1fe56e",
+    "url": "/gear-optimizer/static/media/Wanderer's Chest.eb118d3a.png"
+  },
+  {
+    "revision": "6607c3d248bd43ba16250df19eae84e2",
+    "url": "/gear-optimizer/static/media/Wanderer's Hat.6607c3d2.png"
+  },
+  {
+    "revision": "092d677b84c585bbc967a7f7140f7771",
+    "url": "/gear-optimizer/static/media/Wanderer's Pants.092d677b.png"
+  },
+  {
+    "revision": "2b8990ce93789fd289bf8c4ac05ee19f",
+    "url": "/gear-optimizer/static/media/Yellow Heart.2b8990ce.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gear-optimizer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7ce0dbce3f42a880fae973e667ba2756",
+    "url": "/gear-optimizer/static/media/stnaP s'rerednaW.7ce0dbce.png"
+  },
+  {
+    "revision": "f6ac2d3a81b33ddb4cbd62586860f8af",
+    "url": "/gear-optimizer/static/media/stooB s'rerednaW.f6ac2d3a.png"
+  },
+  {
+    "revision": "8d2a6cefe4adc2591b37a4dec4137015",
+    "url": "/gear-optimizer/static/media/taH s'rerednaW.8d2a6cef.png"
+  },
+  {
+    "revision": "215269fea44bd80e1508ea2b1846df23",
+    "url": "/gear-optimizer/static/media/tsehC s'rerednaW.215269fe.png"
+  }
+]);
