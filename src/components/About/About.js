01fa59a5f740890d70404f15d0161afcccfb9877
Giles Miclotte <giles.miclotte@ugent.be>
2019-07-26 00:05:18 +0200

    Fix automated versioning

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,11 +3,12 @@ import ReactGA from 'react-ga';
 import './About.css';
 
 import GitCommit from '../../_git_commit';
+import GOVersion from '../../_version';
 
 const About = ({className}) => {
         ReactGA.pageview('/about');
         return <p className="center">
-                {'NGU Idle Gear Optimizer v' + process.env.REACT_APP_VERSION}
+                {'NGU Idle Gear Optimizer v' + GOVersion.version}
                 <br/> {'Git hash: ' + GitCommit.logMessage.slice(0, 8)}
                 <br/>
                 <a href="https://github.com/gmiclotte/gear-optimizer/issues/new" rel="noopener noreferrer" target="_blank">
