c51b9a59561c9290de5ef4f82bd669623552d9e9
Giles Miclotte <giles.miclotte@ugent.be>
2019-07-25 23:48:06 +0200

    Change versioning

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,15 +3,12 @@ import ReactGA from 'react-ga';
 import './About.css';
 
 import GitCommit from '../../_git_commit';
-import {generate, parse} from 'build-number-generator';
-const buildTime = parse(generate());
 
 const About = ({className}) => {
         ReactGA.pageview('/about');
         return <p className="center">
-                {'NGU Idle Gear Optimizer'}
+                {'NGU Idle Gear Optimizer v' + process.env.REACT_APP_VERSION}
                 <br/> {'Git hash: ' + GitCommit.logMessage.slice(0, 8)}
-                <br/> {'Updated on:   ' + buildTime.toISOString()}
                 <br/>
                 <a href="https://github.com/gmiclotte/gear-optimizer/issues/new" rel="noopener noreferrer" target="_blank">
                         Report an issue.
