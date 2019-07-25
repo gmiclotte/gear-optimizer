8c85a48d42873d1f0b6f2b4d55871567a3b9e472
Giles Miclotte <giles.miclotte@ugent.be>
2019-07-25 23:10:17 +0200

    Add version and update time to about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import ReactGA from 'react-ga';
-
 import './About.css';
+
+import GitCommit from '../../_git_commit';
+import {generate, parse} from 'build-number-generator';
+const buildTime = parse(generate());
 
 const About = ({className}) => {
         ReactGA.pageview('/about');
         return <p className="center">
                 {'NGU Idle Gear Optimizer'}
-                <br/> {'Not affiliated with '}
-                <a href="https://www.kongregate.com/games/somethingggg/ngu-idle" rel="noopener noreferrer" target="_blank">
-                        NGU Idle
-                </a>{'.'}
-                <br/> {'All art copyright by '}
-                <a href="https://www.kongregate.com/accounts/somethingggg" rel="noopener noreferrer" target="_blank">
-                        4G
-                </a>{'.'}
+                <br/> {'Git hash: ' + GitCommit.logMessage.slice(0, 8)}
+                <br/> {'Updated on:   ' + buildTime.toISOString()}
                 <br/>
                 <a href="https://github.com/gmiclotte/gear-optimizer/issues/new" rel="noopener noreferrer" target="_blank">
                         Report an issue.
                 </a>
+                <br/>
+                <br/> {'Not affiliated with '}
+                <a href="https://www.kongregate.com/games/somethingggg/ngu-idle" rel="noopener noreferrer" target="_blank">
+                        NGU Idle
+                </a>{'.'}
+                <br/>
+                <br/> {'All art copyright by '}
+                <a href="https://www.kongregate.com/accounts/somethingggg" rel="noopener noreferrer" target="_blank">
+                        4G
+                </a>{'.'}
         </p>;
 };
 
