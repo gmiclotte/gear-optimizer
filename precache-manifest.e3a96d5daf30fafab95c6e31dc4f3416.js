784f14585f07f15b441f6ba9f177baf14d4c1bdb
Giles Miclotte <giles.miclotte@ugent.be>
2020-03-14 19:58:31 +0100

    Updates

---
+++ precache-manifest.e3a96d5daf30fafab95c6e31dc4f3416.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b969323cfac9993625433c482f634b2",
+    "url": "/gear-optimizer/b818d807356719b9fa1a.worker.js"
+  },
+  {
+    "revision": "70d27fb6cabf986c57a3f7c2c76c4f19",
+    "url": "/gear-optimizer/index.html"
+  },
+  {
+    "revision": "d1e15ac4337a6b7de858",
+    "url": "/gear-optimizer/static/css/main.029a50cb.chunk.css"
+  },
+  {
+    "revision": "f542a20b2e5f8e7c6172",
+    "url": "/gear-optimizer/static/js/2.7eac685a.chunk.js"
+  },
+  {
+    "revision": "23f5a830e8a7411b5351acd012ed2d97",
+    "url": "/gear-optimizer/static/js/2.7eac685a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e15ac4337a6b7de858",
+    "url": "/gear-optimizer/static/js/main.28d9bd0d.chunk.js"
+  },
+  {
+    "revision": "ad6feb7919cd4fd71138",
+    "url": "/gear-optimizer/static/js/runtime-main.e2a030b9.js"
+  }
+]);
