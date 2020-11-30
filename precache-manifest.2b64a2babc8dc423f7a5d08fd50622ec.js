ca8b925700fe813fff3ee9bff5fce15c6da225d7
Giles Miclotte <Giles.Miclotte@GMail.com>
2020-11-30 09:22:24 +0100

    Updates

---
+++ precache-manifest.2b64a2babc8dc423f7a5d08fd50622ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2561839f75c9d19bbc61c121ca9aae49",
+    "url": "/gear-optimizer/550e027fc22408c6ce14.worker.js"
+  },
+  {
+    "revision": "bdef4a2357ceb131970a604492b514c4",
+    "url": "/gear-optimizer/index.html"
+  },
+  {
+    "revision": "3034eaf2d94371372eb0",
+    "url": "/gear-optimizer/static/css/main.505aaad3.chunk.css"
+  },
+  {
+    "revision": "560a3dd9cbba48b0fb67",
+    "url": "/gear-optimizer/static/js/2.6c80861d.chunk.js"
+  },
+  {
+    "revision": "ee382b57c195b3f1f0f7cae9e631e2c3",
+    "url": "/gear-optimizer/static/js/2.6c80861d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3034eaf2d94371372eb0",
+    "url": "/gear-optimizer/static/js/main.e1e9da76.chunk.js"
+  },
+  {
+    "revision": "dc1c676ba897cc436e9f",
+    "url": "/gear-optimizer/static/js/runtime-main.0d730cbe.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gear-optimizer/precache-manifest.82dbd75efc5fa28798b2d057f14a3a3c.js"
+  "/gear-optimizer/precache-manifest.2b64a2babc8dc423f7a5d08fd50622ec.js"
 );
 
 self.addEventListener('message', (event) => {
