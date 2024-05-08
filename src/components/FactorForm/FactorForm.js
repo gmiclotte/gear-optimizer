8d7873dad3e6e1cd480b0a7f9d33e38a62973ec1
pandahaiku <douglasgalm@gmail.com>
2024-05-07 18:40:07 -0700

    Add searchable field for optimization factors

--- src/components/FactorForm/FactorForm.js
+++ src/components/FactorForm/FactorForm.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import Select from 'react-select';
 
-import {Factors} from '../../assets/ItemAux'
-import {default as Crement} from '../Crement/Crement';
+import { Factors } from '../../assets/ItemAux';
+import { default as Crement } from '../Crement/Crement';
 
 export default class FactorForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: Factors[this.props.factors[this.props.idx]][0]
+            selectedOption: { value: Factors[this.props.factors[this.props.idx]][0], label: Factors[this.props.factors[this.props.idx]][0] }
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
-        this.props.handleEditFactor(this.props.idx, Object.getOwnPropertyNames(Factors).filter((factor) => (Factors[factor][0] === event.target.value))[0])
+    handleChange(selectedOption) {
+        this.setState({ selectedOption });
+        this.props.handleEditFactor(this.props.idx, Object.getOwnPropertyNames(Factors).find(factor => Factors[factor][0] === selectedOption.value));
     }
 
     handleSubmit(event) {
@@ -23,23 +24,73 @@ export default class FactorForm extends React.Component {
     }
 
     render() {
-        let factor = Factors[this.props.factors[this.props.idx]];
-        //HACK: this sets the dropdown to the correct value after loading
-        if (this.state.value !== factor[0]) {
-            /* eslint-disable-next-line react/no-direct-mutation-state */
-            this.state.value = factor[0];
-        }
+        const factor = Factors[this.props.factors[this.props.idx]];
+        const options = Object.getOwnPropertyNames(Factors).map(factor => ({
+            value: Factors[factor][0],
+            label: Factors[factor][0]
+        }));
+        
+        const selectedValue = options.find(option => option.value === factor[0]);
+    
         const accslots = this.props.equip.accessory.length;
-        return (<label key={this.props.factors[this.props.idx]}>
-            {'Priority '}
-            {Number(this.props.idx) + 1}
-            {': '}
-            <select value={this.state.value} onChange={this.handleChange}>
-                {Object.getOwnPropertyNames(Factors).map((factor) => (
-                    <option value={Factors[factor][0]} key={Factors[factor][0]}>{Factors[factor][0]}</option>))}
-            </select>
-            <Crement header='slots' value={this.props.maxslots[this.props.idx]} name={['maxslots', this.props.idx]}
-                     handleClick={this.props.handleCrement} min={0} max={accslots}/>
-        </label>);
+    
+        return (
+            <div style={{ display: 'flex', alignItems: 'flex-start', flexDirection: 'column', marginTop: '5px'}}>
+                <div style={{ display: 'flex', alignItems: 'flex-start', flexDirection: 'row'}}>
+                    <label style={{ marginRight: '5px', fontSize: '14px' }}>
+                        {'Priority '}
+                        {Number(this.props.idx) + 1}
+                        {': '}
+                    </label>
+                    <Select
+                        value={selectedValue}
+                        onChange={this.handleChange}
+                        options={options}
+                        styles={customStyles}
+                        className="basic-single"
+                        classNamePrefix="select"
+                    />
+                </div>
+                <div>
+                    <Crement
+                        header='slots'
+                        value={this.props.maxslots[this.props.idx]}
+                        name={['maxslots', this.props.idx]}
+                        handleClick={this.props.handleCrement}
+                        min={0}
+                        max={accslots}
+                    />
+                </div>
+            </div>
+        );
     }
 }
+
+const customStyles = {
+    control: (provided) => ({
+        ...provided,
+        minHeight: '20px',
+        height: '20px',
+        width: '200px' 
+    }),
+    singleValue: (provided) => ({
+        ...provided,
+        fontSize: '14px',
+        lineHeight: '14px',
+        paddingBottom: '12px'
+    }),
+    input: (provided) => ({
+        ...provided,
+        fontSize: "14px",
+        lineHeight: "0px",
+        padding: "0px",
+    }),
+    indicatorsContainer: (provided) => ({
+        ...provided,
+        height: '20px'
+    }),
+    option: (provided) => ({
+        ...provided,
+        fontSize: '12px'
+    })
+};
